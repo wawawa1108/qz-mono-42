639cc6f01e70cfdad48f61b4bb3adf2c915c6f11
wawawa1108 <aokuma1911@gmail.com>
2026-07-05 20:42:43 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'bulk-tracker-v1';
+const ASSETS = ['./bulk-nutrition-tracker-pwa.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
